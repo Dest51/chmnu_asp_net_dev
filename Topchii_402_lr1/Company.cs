a0464d4f6ed0f451433d69c8f3ee5c5bed89f1b0
Dest51 <dest5122@gmail.com>
2024-02-05 16:40:57 +0200

    trash files deleted

---
+++ Topchii_402_lr1/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Topchii_402_lr1.Models
+{
+	public class Company
+	{
+        public string Name { get; set; }
+        public string Industry { get; set; }
+        public string Location { get; set; }
+        public int EmployeesCount { get; set; }
+
+        //public Company()
+        //{
+        //}
+    }
+}
+
---
+++ Topchii_402_lr1/Controllers/CompanyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Topchii_402_lr1.Models;
+
+namespace Topchii_402_lr1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CompanyController : ControllerBase
+{
+    private readonly Company _company;
+
+    public CompanyController()
+    {
+        _company = new Company
+        {
+            Name = "ArTem inc.",
+            Industry = "App development",
+            Location = "Ukraine",
+            EmployeesCount = 23
+
+        };
+    }
+
+    [HttpGet]
+    public ActionResult GetCompanyInfo()
+    {
+        return Ok(new
+        {
+            Name = _company.Name,
+            Industry = _company.Industry,
+            Location = _company.Location,
+            EmployeesCount = _company.EmployeesCount
+        });
+    }
+
+    [HttpGet("randNumber")]
+    public IActionResult GetRandomNumber()
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(0, 101);
+        return Ok(randomNumber);
+    }
+}
+
