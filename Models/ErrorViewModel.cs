0a46e284cd0f4b58ba295442392c2e194eef19ec
Dest51 <dest5122@gmail.com>
2024-03-18 16:45:34 +0200

    Uploaded files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Topchii_402_lr7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
