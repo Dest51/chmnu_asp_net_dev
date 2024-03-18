7f9a88a084696d59f9cefcda2c716aa436fd788b
Dest51 <dest5122@gmail.com>
2024-03-18 16:46:03 +0200

    File uploaded

---
+++ Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Topchii_402_lr7.Models;
+
+namespace Topchii_402_lr7.Controllers;
+
+public class FileController : Controller
+{
+    private readonly ILogger<FileController> _logger;
+
+    public FileController(ILogger<FileController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult DownloadFile()
+    {
+        return View(new FileViewModel()); 
+    }
+
+    [HttpPost]
+    public IActionResult DownloadFile(FileViewModel model)
+    {
+        // Генеруємо вміст файлу
+        string fileContent = $"Ім'я: {model.FirstName}\nПрізвище: {model.LastName}";
+
+        // Записуємо вміст файлу
+        byte[] fileBytes = Encoding.UTF8.GetBytes(fileContent);
+        return File(fileBytes, "text/plain", $"{model.FileName}.txt");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Models/FileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Topchii_402_lr7.Models
+{
+	public class FileViewModel
+	{
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FileName { get; set; }
+    }
+}
+
